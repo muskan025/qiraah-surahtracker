430ff863e886d7fe2d6f61d2c730cb55d16c5420
Muskan <102637258+muskan025@users.noreply.github.com>
2024-10-02 09:15:26 +0530

    Update slice.js

--- qiraah-client/redux-toolkit/slice.js
+++ qiraah-client/redux-toolkit/slice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const api = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:8000",
+    baseUrl: "https://qiraah-backend.vercel.app",
     credentials: 'include',
   }),
   tagTypes: ["Playlist"],
