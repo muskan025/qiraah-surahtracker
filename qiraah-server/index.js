0f1c234fd446cf9e9ca07bff99d6fc79e1880733
Muskan <102637258+muskan025@users.noreply.github.com>
2024-10-02 10:35:33 +0530

    Update index.js

--- qiraah-server/index.js
+++ qiraah-server/index.js
@@ -14,7 +14,7 @@ app.use(fileUpload({
     limits: { fileSize: 50 * 1024 * 1024 }, 
   }));
 app.use(cors({
-    origin: 'http://localhost:5173', 
+    origin: 'https://qiraah.vercel.app', 
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
